266c0a9b4e0fcab6d78eaacd2f4d84005f1a3b1a
schahaha <shicheng31604@gmail.com>
2019-04-02 23:01:09 +0800

    2019.04.02 晚上在家里完成

--- src/02_knn/01_dating.py
+++ src/02_knn/01_dating.py
@@ -1,9 +1,10 @@
 # 本文主要用于编写海伦约会配对数据
-from numpy.matlib import zeros
+from numpy.matlib import zeros, array
 
 import sys
 
 
+# 将每一行数据的前三个作为特征，最后一个数字作为标签
 def fileToMatrix(filename):
     fr = open(filename)
     length = len(fr.readlines())
@@ -14,6 +15,7 @@ def fileToMatrix(filename):
     index=0
 
     for line in fr.readlines():
+
         split = line.split("\t")
 
         returnMat[index,:]=split[0:3]
@@ -27,12 +29,30 @@ def fileToMatrix(filename):
 
 if __name__ == '__main__':
     (mat,classLabelVector) = fileToMatrix("../../resources/02_knn/datingTestSet2.txt")
+    #
+    # print(classLabelVector)
 
 
     import matplotlib.pyplot as plt
 
     figure = plt.figure()
     ax = figure.add_subplot(111)
+
+    # 将第一列的和第二列单独拿出来做对比，分别将对应不同z值的点化成不同的颜色
+    mat1=[]
+    label1=[]
+
+    index =0
+    for x in mat[:,0]:
+        if classLabelVector[index]==1:
+            mat1.append(mat1)
+
+    # 这里的x，y需要使用中括号括起来，不知道是为啥？API的变动？
+    ax.scatter([mat[:, 0]], [mat[:, 1]],cmap=classLabelVector)
+
+    plt.show()
+
+#     todo 继续将图绘制完毕
 
 
 
